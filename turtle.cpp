b1ff5685e8c30a989614a67cbb5f0de88c641529
yarlagaddavenkatasubbarao <yarlagadda.venkat.chinni@gmail.com>
2021-01-11 09:06:54 -0600

    Add files via upload

---
+++ turtle.cpp
@@ -0,0 +1,23 @@
+#include<simplecpp>
+
+main_program{
+turtleSim();
+forward(100); right(90); wait(0.5);
+forward(100); right(90); wait(0.5);
+forward(100); right(90); wait(0.5);
+forward(100); wait(5);
+
+forward(50); right(60); wait(0.5);
+forward(50); right(60); wait(0.5);
+forward(50); right(60); wait(0.5);
+forward(50); wait(5);
+forward(100); right(90); wait(0.5);
+forward(100); right(90); wait(0.5);
+forward(100); right(90); wait(0.5);
+forward(100); wait(5);
+forward(100); right(90); wait(0.5);
+forward(100); right(90); wait(0.5);
+forward(100); right(90); wait(0.5);
+forward(100); wait(5);
+
+}
